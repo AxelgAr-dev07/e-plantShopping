903fee2d8bcbb7da6345083ec4bce8a3e6885229
Axel Armas <axelrub07@gmail.com>
2026-01-12 09:48:16 -0600

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -212,29 +212,6 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
-     return (
-             <div className="product-grid">
-                 {plantsArray.map((categoryItem, categoryIndex) => (
-                    <div key={categoryIndex}>
-                       <h2>{categoryItem.category}</h2>
-                    <div className="category-grid">
-                          {categoryItem.plants.map((plant, plantIndex) => (
-                             <div key={plantIndex} className="product-card">
-                                <h3>{plant.name}</h3>
-                                <img
-                                    src={plant.image}
-                                    alt={plant.name}
-                                    className="product-image"
-                                 />
-                                <p>{plant.description}</p>
-                                <p><strong>Cost:</strong> {plant.cost}</p>
-                            </div>
-                                   ))}
-                            </div>
-                      </div>
-                  ))}
-           </div>
-        );
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -297,7 +274,29 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
+                 return (
+             <div className="product-grid">
+                 {plantsArray.map((categoryItem, categoryIndex) => (
+                    <div key={categoryIndex}>
+                       <h2>{categoryItem.category}</h2>
+                    <div className="category-grid">
+                          {categoryItem.plants.map((plant, plantIndex) => (
+                             <div key={plantIndex} className="product-card">
+                                <h3>{plant.name}</h3>
+                                <img
+                                    src={plant.image}
+                                    alt={plant.name}
+                                    className="product-image"
+                                 />
+                                <p>{plant.description}</p>
+                                <p><strong>Cost:</strong> {plant.cost}</p>
+                            </div>
+                                   ))}
+                            </div>
+                      </div>
+                  ))}
+           </div>
+        );
 
                 </div>
             ) : (
